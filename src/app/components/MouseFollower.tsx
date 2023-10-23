47ae20a2344ae4d10e02a9f8d6e7e63b63ff22fe
Youssef Ahmed <youssefashwal@outlook.com>
2023-10-23 08:49:42 +0300

    Created MouseFollower component

---
+++ src/app/components/MouseFollower.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import styles from "@styles/mouse_follower.module.scss";
+import { useEffect, useState } from "react";
+
+export default function MouseFollower() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState(false);
+
+  const size = 150;
+
+  useEffect(() => {
+    const setFromEvent = (e: MouseEvent) => {
+      if (!isVisible) setIsVisible(true);
+
+      let newY = e.clientY - size / 2;
+      let newX = e.clientX - size / 2;
+
+      const headerElement = document.querySelector("header");
+
+      if (headerElement) {
+        const headerRect = headerElement.getBoundingClientRect();
+
+        const maxTopPositionAllowed = headerRect.bottom - (size * 2) / 3.5;
+
+        if (newY < maxTopPositionAllowed) {
+          newY = maxTopPositionAllowed;
+        }
+      }
+
+      setPosition({ x: newX, y: newY });
+    };
+
+    window.addEventListener("mousemove", setFromEvent);
+
+    return () => {
+      window.removeEventListener("mousemove", setFromEvent);
+    };
+  }, []);
+
+  return (
+    <div
+      className={styles.mouseFollower}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        opacity: isVisible ? 1 : 0,
+      }}
+    />
+  );
+}
