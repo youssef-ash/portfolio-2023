e4fbf4c273914dc081606f3d845fc6f158a8494d
Youssef Ahmed <youssefashwal@outlook.com>
2023-11-09 22:13:26 +0200

    Switched Links to anchor tags in Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,24 @@
 import styles from "@styles/header.module.scss";
-import Link from "next/link";
+
+import { BsSunFill } from "react-icons/bs";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <button className={styles.themeButton}>Theme Switcher</button>
-      <nav>
-        <ul className={styles.list}>
-          <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About</Link>
-          <Link href={"/work"}>Work</Link>
-          <Link href={"/contact"}>Contact</Link>
-        </ul>
-      </nav>
-      <button className={styles.ctaButton}>Hire Me</button>
+      <div className={styles.container}>
+        <button className={styles.themeButton}>
+          <BsSunFill size={"1.5em"} />
+        </button>
+        <nav>
+          <ul className={styles.list}>
+            <a href={"#home"}>Home</a>
+            <a href={"#work"}>Work</a>
+            <a href={"/about"}>About</a>
+            <a href={"/contact"}>Contact</a>
+          </ul>
+        </nav>
+        <button className={styles.ctaButton}>Hire Me</button>
+      </div>
     </header>
   );
 }
