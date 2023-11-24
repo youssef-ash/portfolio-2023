316ad6d9a71e85b7ebe2f54dbec8c92728e5e9dc
Youssef Ahmed <youssefashwal@outlook.com>
2023-11-24 23:50:20 +0200

    Created Project util component

---
+++ src/app/components/utils/Project.tsx
@@ -0,0 +1,48 @@
+import styles from "@styles/work.module.scss";
+
+import { ProjectImagePlacementT } from "@utils/types";
+import Image, { StaticImageData } from "next/image";
+
+interface ProjectProps {
+  imagePlacement: ProjectImagePlacementT;
+  title: string;
+  description: string;
+  liveLink: string;
+  githubLink: string;
+  imageSrc: StaticImageData;
+}
+
+export default function Project({
+  imagePlacement,
+  title,
+  description,
+  liveLink,
+  githubLink,
+  imageSrc,
+}: ProjectProps) {
+  return (
+    <div
+      className={`${styles.project} ${
+        imagePlacement === "left" ? styles.flipped : ""
+      } `}
+    >
+      <span className={styles.type}>
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <div className={styles.buttonsContainer}>
+          <button className={styles.liveButton}>Live Site</button>
+          <button className={styles.githubButton}>Code</button>
+        </div>
+      </span>
+      <span className={styles.imgContainer}>
+        <Image
+          src={imageSrc}
+          alt="project image"
+          fill={true}
+          sizes=""
+          draggable={false}
+        />
+      </span>
+    </div>
+  );
+}
