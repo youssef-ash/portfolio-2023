3c4dc1f36409c0ed7032d40dc1335fade0b533b6
Youssef Ahmed <youssefashwal@outlook.com>
2024-01-10 00:54:25 +0200

    Turned inputs to controlled inputs and added anchor links in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FaPaperPlane,
   FaEnvelope,
@@ -8,53 +9,87 @@ import {
 import styles from "@styles/contact.module.scss";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const validateEmail = (email: string) => {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+  };
+
+  const fieldsFilled =
+    name.trim() !== "" && validateEmail(email) && message.trim() !== "";
+
   return (
     <section id="contact" className={styles.container}>
-      {" "}
       <h3 className={styles.title}>Contact Me</h3>
       <div className={styles.subContainer}>
         <form className={styles.form}>
           <div className={styles.fieldGroup}>
             <div className={styles.field}>
               <label htmlFor="name">Your name:</label>
-              <input id="name"></input>
+              <input
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
             </div>
             <div className={styles.field}>
               <label htmlFor="email">Your email:</label>
-              <input id="email"></input>
+              <input
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
             </div>
           </div>
           <div className={styles.field}>
             <label htmlFor="message">Your message:</label>
-            <textarea id="message"></textarea>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </div>
 
-          <button>
+          <button disabled={!fieldsFilled}>
             <FaPaperPlane />
             Send
           </button>
         </form>
 
         <div className={styles.linksContainer}>
-          <div className={styles.link}>
+          <a
+            className={styles.link}
+            href="mailto:youssefashwal@outlook.com"
+            target="_blank"
+          >
             <FaEnvelope size={"1.3em"} />
             youssefashwal@outlook.com
-          </div>
+          </a>
           <div className={styles.separator}>OR</div>
 
           <div className={styles.linksGroup}>
-            <div className={styles.link}>
+            <a
+              className={styles.link}
+              href="https://www.linkedin.com/in/youssef-ash/"
+              target="_blank"
+            >
               <FaLinkedin size={"1.3em"} />
               My Linkedin
-            </div>
+            </a>
             <div className={styles.link}>
               <FaPhone size={"1.2em"} />
               01111162356
             </div>
-            <div className={styles.link}>
+            <a
+              className={styles.link}
+              href="https://github.com/youssef-ash"
+              target="_blank"
+            >
               <FaGithub size={"1.3em"} />
               My Github
-            </div>
+            </a>
           </div>
         </div>
       </div>
