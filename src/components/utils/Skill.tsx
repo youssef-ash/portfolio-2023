e76aa7e2de29aa875f93fcc44f2298bf5d0c0126
Youssef Ahmed <youssefashwal@outlook.com>
2024-01-23 02:03:37 +0200

    Added animation to Skill util component

--- src/components/utils/Skill.tsx
+++ src/components/utils/Skill.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import { Variants, motion } from "framer-motion";
 
 interface SkillProps {
   imageSrc: string;
   name: string;
+  skillVariant: Variants;
 }
 
-export default function Skill({ imageSrc, name }: SkillProps) {
+export default function Skill({ imageSrc, name, skillVariant }: SkillProps) {
   return (
-    <span>
+    <motion.span variants={skillVariant}>
       <Image src={imageSrc} alt={`${name} logo`} width={0} height={0} />
       <span>{name}</span>
-    </span>
+    </motion.span>
   );
 }
