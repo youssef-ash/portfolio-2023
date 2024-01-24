af95c722a629ac8bf86e970f357102e84added6e
Youssef Ahmed <youssefashwal@outlook.com>
2024-01-25 01:22:08 +0200

    Added animations to Work component

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,39 +1,102 @@
-import styles from "@styles/work.module.scss";
-import Project from "./utils/Project";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
+import SectionTitle from "@componentUtils/SectionTitle";
+import Project from "@componentUtils/Project";
 import Image from "/public/images/nile_visits/nile_visits1.png";
+import styles from "@styles/work.module.scss";
+
+const projectVariant = {
+  hidden: { opacity: 0, y: -30 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 75 },
+  },
+};
+
+const MotionProject = motion(Project);
 
 export default function Work() {
-  return (
-    <section id="work" className={styles.container}>
-      <h3 className={styles.title}>My Work</h3>
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
 
-      <div className={styles.projects}>
-        <Project
+  return (
+    <section id="work" className={styles.container} ref={ref}>
+      <SectionTitle
+        className={styles.title}
+        text="My Work"
+        inView={inView}
+        delay={2.1}
+      />
+
+      <motion.div
+        className={styles.projects}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7, delay: 2.6 }}
+      >
+        <MotionProject
           imagePlacement="right"
+          title="Nile Orientations App"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          liveLink=""
+          githubLink=""
+          imageSrc={Image}
+          variants={projectVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+        />
+        <MotionProject
+          imagePlacement="left"
           title="Nile Visits App"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
           liveLink=""
           githubLink=""
           imageSrc={Image}
+          variants={projectVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.35 }}
         />
-        <Project
-          imagePlacement="left"
-          title="Title"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          liveLink=""
-          githubLink=""
-          imageSrc={Image}
-        />
-        <Project
+        <MotionProject
           imagePlacement="right"
-          title="Title"
+          title="Online Veterinary Clinic"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
           liveLink=""
           githubLink=""
           imageSrc={Image}
+          variants={projectVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.35 }}
         />
-      </div>
+        <MotionProject
+          imagePlacement="left"
+          title="My Portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          liveLink=""
+          githubLink=""
+          imageSrc={Image}
+          variants={projectVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.35 }}
+        />
+        <MotionProject
+          imagePlacement="right"
+          title="Trivia App"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          liveLink=""
+          githubLink=""
+          imageSrc={Image}
+          variants={projectVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.35 }}
+        />
+      </motion.div>
     </section>
   );
 }
--- src/components/utils/Project.tsx
+++ src/components/utils/Project.tsx
@@ -1,7 +1,7 @@
-import styles from "@styles/work.module.scss";
-
-import { ProjectImagePlacementT } from "../../utils/types";
+import { ProjectImagePlacementT } from "@utils/types";
 import Image, { StaticImageData } from "next/image";
+import styles from "@styles/work.module.scss";
+import { Ref, forwardRef } from "react";
 
 interface ProjectProps {
   imagePlacement: ProjectImagePlacementT;
@@ -12,41 +12,52 @@ interface ProjectProps {
   imageSrc: StaticImageData;
 }
 
-export default function Project({
-  imagePlacement,
-  title,
-  description,
-  liveLink,
-  githubLink,
-  imageSrc,
-}: ProjectProps) {
-  return (
-    <div
-      className={`${styles.project} ${
-        imagePlacement === "left" ? styles.flipped : ""
-      } `}
-    >
-      <span className={styles.type}>
-        <h4>{title}</h4>
-        <p>{description}</p>
-        <div className={styles.buttonsContainer}>
-          <button className={styles.liveButton} disabled={liveLink === ""}>
-            Live Site
-          </button>
-          <button className={styles.githubButton} disabled={githubLink === ""}>
-            Code
-          </button>
-        </div>
-      </span>
-      <span className={styles.imgContainer}>
-        <Image
-          src={imageSrc}
-          alt="project image"
-          fill={true}
-          sizes=""
-          draggable={false}
-        />
-      </span>
-    </div>
-  );
-}
+const Project = forwardRef(
+  (
+    {
+      imagePlacement,
+      title,
+      description,
+      liveLink,
+      githubLink,
+      imageSrc,
+    }: ProjectProps,
+    ref: Ref<HTMLDivElement>
+  ) => {
+    return (
+      <div
+        className={`${styles.project} ${
+          imagePlacement === "left" ? styles.flipped : ""
+        } `}
+        ref={ref}
+      >
+        <span className={styles.type}>
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <div className={styles.buttonsContainer}>
+            <button className={styles.liveButton} disabled={liveLink === ""}>
+              Live Site
+            </button>
+            <button
+              className={styles.githubButton}
+              disabled={githubLink === ""}
+            >
+              Code
+            </button>
+          </div>
+        </span>
+        <span className={styles.imgContainer}>
+          <Image
+            src={imageSrc}
+            alt="project image"
+            fill={true}
+            sizes=""
+            draggable={false}
+          />
+        </span>
+      </div>
+    );
+  }
+);
+
+export default Project;
