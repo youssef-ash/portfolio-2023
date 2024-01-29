3694adcfa6d15a10fdb0fcf22f1f3e8babf97480
Youssef Ahmed <youssefashwal@outlook.com>
2024-01-29 09:24:58 +0200

    Updated nile visits project images

--- src/utils/projectImages/nileVisits/nileVisits.ts
+++ src/utils/projectImages/nileVisits/nileVisits.ts
@@ -3,7 +3,23 @@ import image2 from "./visits2.png";
 import image3 from "./visits3.png";
 import image4 from "./visits4.png";
 import image5 from "./visits5.png";
+import image6 from "./visits6.png";
+import image7 from "./visits7.png";
+import image8 from "./visits8.png";
+import image9 from "./visits9.png";
+import image10 from "./visits10.png";
 
-const nileVisitsImages = [image1, image2, image3, image4, image5];
+const nileVisitsImages = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+];
 
 export default nileVisitsImages;
