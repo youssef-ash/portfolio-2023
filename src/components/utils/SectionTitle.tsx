9429fb6e83ad00a67c5455f2d30c78b429a0fd3e
Youssef Ahmed <youssefashwal@outlook.com>
2024-02-03 05:02:48 +0200

    Updated animation for SectionTitle util component

--- src/components/utils/SectionTitle.tsx
+++ src/components/utils/SectionTitle.tsx
@@ -3,15 +3,22 @@ import { motion } from "framer-motion";
 interface SectionTitleProps {
   className: string;
   text: string;
+  inView: boolean;
+  delay?: number;
 }
 
-export default function SectionTitle({ className, text }: SectionTitleProps) {
+export default function SectionTitle({
+  className,
+  text,
+  inView,
+  delay = 0,
+}: SectionTitleProps) {
   return (
     <motion.h3
       className={className}
-      initial={{ opacity: 0, y: "-1.5em" }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ ease: "easeInOut", duration: 0.7 }}
+      initial={{ opacity: 0, y: -30 }}
+      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -30 }}
+      transition={{ ease: "easeInOut", duration: 0.7, delay: delay }}
     >
       {text}
     </motion.h3>
