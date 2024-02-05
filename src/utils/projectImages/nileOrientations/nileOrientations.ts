c95f28d8fb217d0e61b2c696e0d6318d901f7e65
Youssef Ahmed <youssefashwal@outlook.com>
2024-02-05 07:22:01 +0200

    Commented out nileOrientations export temporarily

--- src/utils/projectImages/nileOrientations/nileOrientations.ts
+++ src/utils/projectImages/nileOrientations/nileOrientations.ts
@@ -1,9 +1,3 @@
-import image1 from "./orientations1.png";
-import image2 from "./orientations2.png";
-import image3 from "./orientations3.png";
-import image4 from "./orientations4.png";
-import image5 from "./orientations5.png";
+// const nileOrientationsImages = [];
 
-const nileOrientationsImages = [image1, image2, image3, image4, image5];
-
-export default nileOrientationsImages;
+// export default nileOrientationsImages;
